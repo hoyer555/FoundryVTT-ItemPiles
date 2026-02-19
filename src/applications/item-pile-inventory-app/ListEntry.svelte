5cbeff3f88e426c1ca1c4941d2e0bbb013dbfeb3
femapj <federico.negri555@gmail.com>
2026-02-19 15:39:15 +0100

    test ListEntry.svelte

--- src/applications/item-pile-inventory-app/ListEntry.svelte
+++ src/applications/item-pile-inventory-app/ListEntry.svelte
@@ -47,7 +47,7 @@
 			   on:click={() => { entry.preview() }}
 			   style="color: {$rarityColor || 'inherit'};"
 			>
-				{$name}
+				{$name} <span class="identified-name" data-tooltip="{{this.identifiedName}}"><i class="fas fa-circle-question"></i></span>
 			</p>
 			{#if !$editQuantities && (entry.canStack || !entry.id) && ($pileData.shareCurrenciesEnabled || !currency)}
 				<span class="item-piles-small-text">(x{$quantity})</span>
